8a6b141edb03231214e42bacdf850fc386a6b541
georgiakennedy <georgia.rosekennedy00@gmail.com>
2025-02-11 11:49:45 +1100

    Created schema's for users, posts and comments

---
+++ backend/models/post.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['tips & tricks', 'discussions', 'get inspired', 'resources', 'Self promo'],
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date
+  }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
