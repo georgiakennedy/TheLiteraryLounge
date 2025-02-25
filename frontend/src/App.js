d54f48d62b2cf0689d64986dd1b57480f22e1e6d
georgiakennedy <georgia.rosekennedy00@gmail.com>
2025-02-25 12:52:35 +1100

    Initialised React frontend with basic routing and project structure

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import CreatePostPage from './pages/CreatePostPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* Navbar */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile/:id?" element={<ProfilePage />} />
+        <Route path="/create-post" element={<CreatePostPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
