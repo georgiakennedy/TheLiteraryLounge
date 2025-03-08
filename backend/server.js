edff54746b16c82ce71a6c6fdeb96ac9b6fc3ea8
georgiakennedy <georgia.rosekennedy00@gmail.com>
2025-03-09 10:53:40 +1100

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -11,21 +11,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
 app.use('/uploads', express.static('uploads'));
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/posts', require('./routes/postRoutes'));
 app.use('/api/comments', require('./routes/commentRoutes'));
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong', error: err.message });
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong', error: err.message });
 });
 
-app.get('/', (req, res) => {
-    res.send('Backend is running');
-  });
-  
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
